0133db531b685a6cbb1f8ba6d66552716cf3eb25
Mitul Garg <mitul7hero@gmail.com>
2024-10-02 13:37:01 +0530

    Modularised and better readability

--- checkreq.py
+++ checkreq.py
@@ -1,36 +1,40 @@
 """
-
 Read Sample.py and find imported libraries
 Find required Libraries from requirements.txt
-
 """
 
 file_path = 'sample.py'
 req='requirements.txt'
 
 def findimport(file_path):
+
     with open(file_path, 'r') as file:
         first_lines=[]
+
         for line in file:
                 if 'import' in line:
-                    first_lines.append(line.strip()[7:])
-                else:  # Exclude lines with 'import'
-                    break
-        return first_lines
+                    first_lines.append(line.strip()[7:]) #only keep library name in list (remove 'import ')
+                else: 
+                    break 
+
+    return first_lines
 
 
 def findreqlines(req):
-    a=[]
+    reqlines=[]
     libraries= findimport(file_path)
+
     for lib in libraries:
+
         with open(req, 'r') as file2:
             line_number=0
+
             for line in file2:
                 line_number+=1
-                # print(i,line[:len(i)])
-                if (lib.lower())==(line[:len(lib)].lower()):
-                    a.append(line_number)
-    return a
+                if (lib.lower())==(line[:len(lib)].lower()):  #Search if Library is there in requirements.txt
+                    reqlines.append(line_number)
+                    
+    return reqlines
 
 print(findreqlines(req))
 
